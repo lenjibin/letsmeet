14b6b0b2805aedc8f0bab2f8539668f682d97e96
Benji Lin <linb@mit.edu>
2016-01-10 02:28:48 -0500

    add static files

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 var server = app.listen(3000, function () {
@@ -11,4 +12,3 @@ var server = app.listen(3000, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
