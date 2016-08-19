90689c7b6c094adf7a0c1b5d1cc2bd663366f3be
Benji Lin <benjaleelin@gmail.com>
2016-08-19 03:53:52 -0700

    add moment and change styling

--- main/core.js
+++ main/core.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var google = require('googleapis');
 var helpers = require('./helpers');
 
@@ -24,6 +25,10 @@ function findMutualTime(auth1, auth2, calendars1, calendars2, searchLengthInMinu
       var timeBlocks = [startingTimeBlock];
       timeBlocks = applyOccupiedTimeBlocks(timeBlocks, calendar1Events.events);
       timeBlocks = applyOccupiedTimeBlocks(timeBlocks, calendar2Events.events);
+
+      for (var timeBlocks_i = 0; timeBlocks_i < timeBlocks.length; timeBlocks_i++) {
+        timeBlocks[timeBlocks_i] = toSpecialMomentString(timeBlocks[timeBlocks_i].start) + " - " + toSpecialMomentString(timeBlocks[timeBlocks_i].end);
+      }
 
       callback(timeBlocks);
     });
@@ -83,6 +88,16 @@ function findMutualTime(auth1, auth2, calendars1, calendars2, searchLengthInMinu
     } else {
       callback();
     }
+  }
+}
+
+function toSpecialMomentString(dateTimeStr) {
+  var result = moment(dateTimeStr);
+  var calendar = result.calendar();
+  if (calendar.indexOf("/") != -1) {
+    return result.format('MMM Do, h:mm a')
+  } else {
+    return result.calendar();
   }
 }
 
