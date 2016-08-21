c91c5e3fb2dc6393d6a19aacde18743a403cefef
Benji Lin <benjaleelin@gmail.com>
2016-08-21 00:24:20 -0700

    remove background image. rename ng_modules to static/js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/node_modules/moment', express.static('node_modules/moment'));
 app.use('/node_modules/angular', express.static('node_modules/angular'));
-app.use('/ng_modules', express.static('static/ng_modules'));
+app.use(express.static('static/js'));
 app.use(express.static('static/css'));
 app.use(express.static('static/images'));
 
